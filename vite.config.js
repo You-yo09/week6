e6d4587f1d50b199cb08cf8b27c2053b958843e7
You-yo09 <wulilylu0929@gmail.com>
2024-09-08 13:43:17 +0800

    0908new

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/week6_web/',
+  base: '/week6/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
